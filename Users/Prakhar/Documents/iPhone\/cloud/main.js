a03f821b421992d7e96894d3f2fe70ee2e75a13f
Prakhar Garg <prakhargarg@utexas.edu>
2015-10-18 10:19:11 -0400

    deletes tweet after tweeting

--- Users/Prakhar/Documents/iPhone\/cloud/main.js
+++ Users/Prakhar/Documents/iPhone\/cloud/main.js
@@ -12,75 +12,82 @@ Parse.Cloud.afterSave ("Tweets", function(request, response) {
   console.log(handles);
   console.log("The total length is " + totalLength);
 
-  var tweet = String((request.object.get("tweet")));
+  if (140 - totalLength < 58){
+    var tweet = String((request.object.get("tweet")));
 
-  for ( var i = 0; i <= numberOfHandles; i++ ){
-    tweet = tweet +  " @" + handles[i];
+    for ( var i = 0; i <= numberOfHandles; i++ ){
+      tweet = tweet +  " @" + handles[i];
+    }
+
+    console.log(tweet);
+
+
+    var urlLink = 'https://api.twitter.com/1.1/statuses/update.json';
+
+    var postSummary = tweet;
+    var status = oauth.percentEncode(postSummary);
+    var consumerSecret = "RPIdmNtmya0cUvqD2HbWlIumXpKSow2aeXd8gCFfhikC552k7T";
+    var tokenSecret = "RDhPCdppdia6EIZLA4iVITl7a9rLqRxL3TePpQvGOsPo4";
+    var oauth_consumer_key = "jfK3bEVl07VHpRF7Df2Q2xgSW";
+    var oauth_token = "3749894000-qkmgSL35W0DNUbBXb2vcNTA6lJrYos88AJcwnF9";
+
+    var nonce = oauth.nonce(32);
+    var ts = Math.floor(new Date().getTime() / 1000);
+    var timestamp = ts.toString();
+
+    var accessor = {
+      "consumerSecret": consumerSecret,
+      "tokenSecret": tokenSecret
+    };
+
+
+    var params = {
+      "status":postSummary,
+      "oauth_version": "1.0",
+      "oauth_consumer_key": oauth_consumer_key,
+      "oauth_token": oauth_token,
+      "oauth_timestamp": timestamp,
+      "oauth_nonce": nonce,
+      "oauth_signature_method": "HMAC-SHA1"
+    };
+    var message = {
+      "method": "POST",
+      "action": urlLink,
+      "parameters": params
+    };
+
+
+    //lets create signature
+    oauth.SignatureMethod.sign(message, accessor);
+    var normPar = oauth.SignatureMethod.normalizeParameters(message.parameters);
+    console.log("Normalized Parameters: " + normPar);
+    var baseString = oauth.SignatureMethod.getBaseString(message);
+    console.log("BaseString: " + baseString);
+    var sig = oauth.getParameter(message.parameters, "oauth_signature") + "=";
+    console.log("Non-Encode Signature: " + sig);
+    var encodedSig = oauth.percentEncode(sig); //finally you got oauth signature
+    console.log("Encoded Signature: " + encodedSig);
+
+    Parse.Cloud.httpRequest({
+      method: 'POST',
+      url: urlLink,
+      headers: {
+        "Authorization": 'OAuth oauth_consumer_key="'+oauth_consumer_key+'", oauth_nonce=' + nonce + ', oauth_signature=' + encodedSig + ', oauth_signature_method="HMAC-SHA1", oauth_timestamp=' + timestamp + ',oauth_token="'+oauth_token+'", oauth_version="1.0"'
+      },
+      body: "status="+status,
+      success: function(httpResponse) {
+        response.success(httpResponse.text);
+      },
+      error: function(httpResponse) {
+        response.error('Request failed with response ' + httpResponse.status + ' , ' + httpResponse);
+      }
+    });
+
+
+    request.object.destroy({});
+
   }
 
-  console.log(tweet);
-
-
-  var urlLink = 'https://api.twitter.com/1.1/statuses/update.json';
-
-  var postSummary = tweet;
-  var status = oauth.percentEncode(postSummary);
-  var consumerSecret = "RPIdmNtmya0cUvqD2HbWlIumXpKSow2aeXd8gCFfhikC552k7T";
-  var tokenSecret = "RDhPCdppdia6EIZLA4iVITl7a9rLqRxL3TePpQvGOsPo4";
-  var oauth_consumer_key = "jfK3bEVl07VHpRF7Df2Q2xgSW";
-  var oauth_token = "3749894000-qkmgSL35W0DNUbBXb2vcNTA6lJrYos88AJcwnF9";
-
-  var nonce = oauth.nonce(32);
-  var ts = Math.floor(new Date().getTime() / 1000);
-  var timestamp = ts.toString();
-
-  var accessor = {
-    "consumerSecret": consumerSecret,
-    "tokenSecret": tokenSecret
-  };
-
-
-  var params = {
-    "status":postSummary,
-    "oauth_version": "1.0",
-    "oauth_consumer_key": oauth_consumer_key,
-    "oauth_token": oauth_token,
-    "oauth_timestamp": timestamp,
-    "oauth_nonce": nonce,
-    "oauth_signature_method": "HMAC-SHA1"
-  };
-  var message = {
-    "method": "POST",
-    "action": urlLink,
-    "parameters": params
-  };
-
-
-  //lets create signature
-  oauth.SignatureMethod.sign(message, accessor);
-  var normPar = oauth.SignatureMethod.normalizeParameters(message.parameters);
-  console.log("Normalized Parameters: " + normPar);
-  var baseString = oauth.SignatureMethod.getBaseString(message);
-  console.log("BaseString: " + baseString);
-  var sig = oauth.getParameter(message.parameters, "oauth_signature") + "=";
-  console.log("Non-Encode Signature: " + sig);
-  var encodedSig = oauth.percentEncode(sig); //finally you got oauth signature
-  console.log("Encoded Signature: " + encodedSig);
-
-  Parse.Cloud.httpRequest({
-    method: 'POST',
-    url: urlLink,
-    headers: {
-      "Authorization": 'OAuth oauth_consumer_key="'+oauth_consumer_key+'", oauth_nonce=' + nonce + ', oauth_signature=' + encodedSig + ', oauth_signature_method="HMAC-SHA1", oauth_timestamp=' + timestamp + ',oauth_token="'+oauth_token+'", oauth_version="1.0"'
-    },
-    body: "status="+status,
-    success: function(httpResponse) {
-      response.success(httpResponse.text);
-    },
-    error: function(httpResponse) {
-      response.error('Request failed with response ' + httpResponse.status + ' , ' + httpResponse);
-    }
-  });
 
 
 
