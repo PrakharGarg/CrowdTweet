be6b98112b27549948ac7ba35034e19bec2464ac
Prakhar Garg <prakhargarg@utexas.edu>
2015-10-17 20:42:07 -0400

    fixed script to calculate tweet size

--- Users/Prakhar/Documents/iPhone\/cloud/main.js
+++ Users/Prakhar/Documents/iPhone\/cloud/main.js
@@ -1,7 +1,18 @@
 Parse.Cloud.afterSave ("Tweets", function(request, response) {
   console.log(request.object.get("tweet"));
 
-  var tweetLength = 1 +  String((request.object.get("tweet"))).length + String((request.object.get("handle"))).length;
-  console.log(tweetLength);
+  var handleArray = String((request.object.get("handle")));
+
+  var tweetLength = 1 + String((request.object.get("tweet"))).length;
+  var handleLength = 1 + handleArray.length;
+  var numberOfHandles = request.object.get("handle").split(" ").length - 1;
+  var totalLength = tweetLength + handleLength + numberOfHandles;
+
+  console.log("The total length is " + totalLength);
+
+  if (140 - totalLength < 55){
+    // Tweet it out! 
+  }
+
 
 });
